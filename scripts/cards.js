78792de94a6917bd49603046a83df011a3a2d078
Evgeny Rusakov <shoomec@yandex.ru>
2022-03-09 18:21:08 +0500

    Исправлены замечания после 1го ревью. Доработана функция создания карточки, теперь она добавляет нужные собития на нужные интерактивные элементы для каждой созданной карточки.

---
+++ scripts/cards.js
@@ -0,0 +1,9 @@
+//-- Контент карточек --//
+const cardsContent = [
+  { name: 'Екатеринбург', src: './images/ekaterinburg.webp' },
+  { name: 'Евпатория', src: './images/evpatorya.webp' },
+  { name: 'Магнитогорск', src: './images/magnitogorsk.webp' },
+  { name: 'Москва', src: './images/moskow.webp' },
+  { name: 'Уральское Бали', src: './images/ural-baly.webp' },
+  { name: 'Сочи', src: './images/sochi.webp' }
+];

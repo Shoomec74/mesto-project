65575a6a8904922297f5fb4aa968e9a6786ebe73
Evgeny Rusakov <shoomec@yandex.ru>
2022-04-26 17:26:17 +0500

    work version webpack serv

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -11,7 +13,7 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
     compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    port: 8084, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
     open: true // сайт будет открываться сам при запуске npm run dev
   },
   module: {
@@ -26,6 +28,12 @@ module.exports = {
         exclude: '/node_modules/'
       }
       ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: './src/index.html' // путь к файлу index.html
+  }),
+  new CleanWebpackPlugin(),
+]
 }
 
